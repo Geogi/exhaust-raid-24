b68a1ac37aa6678c92cb10693559000f17385e2e
Paul Blouët <1818316+Geogi@users.noreply.github.com>
2019-12-03 18:50:56 +0100

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
     for participants in 1..=24 {
         let adversaries = participants - 1;
-        let winning_tie_chance = winning_tie(adversaries);
         for roll in 1..=99 {
             let all_below_chance = all_below(adversaries, roll);
+            let winning_tie_chance = winning_tie(adversaries, roll);
             let overall_chance = all_below_chance + winning_tie_chance;
             print!("{};", overall_chance);
         }
@@ -12,11 +12,11 @@ fn main() {
 
 }
 
-fn winning_tie(adversaries: i32) -> f64 {
+fn winning_tie(adversaries: i32, roll: i32) -> f64 {
     let mut chance = 0.0;
     for people_tied_with_me in 1..=adversaries {
         let chance_of_tie = f64::powi(1.0 / 99.0, people_tied_with_me)
-            * f64::powi(98.0 / 99.0, adversaries - people_tied_with_me);
+            * all_below(adversaries - people_tied_with_me, roll);
         let chance_of_winning = 1.0 / f64::from(people_tied_with_me + 1);
         chance += chance_of_tie * chance_of_winning;
     }
